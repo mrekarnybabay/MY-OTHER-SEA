7cf3c8d92816feb0c4653efbe2e15033bfe3a41e
idzidz <idzidz@yandex.ru>
2019-12-29 01:57:29 +0300

    TrCategory

---
+++ src/Components/TrCategory.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import type from '../const';
+
+function TrCategory(props) {
+    if (props.type === type.twoWeek) {
+        return (
+            <tr>
+                <th className={"td-row"}>
+                    <div className={"td-name-category"}><p className={"h5"}>{props.categoryName}</p></div>
+                </th>
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+            </tr>
+        );
+    } else if (props.type === type.oneWeek) {
+        return (
+            <tr>
+                <th className={"td-row"}>
+                    <div className={"td-name-category"}><p className={"h5"}>{props.categoryName}</p></div>
+                </th>
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+                <th className={"td-row"} />
+            </tr>
+        );
+    } else {
+        return null;
+    }
+}
+
+export default TrCategory;
