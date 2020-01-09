2d178e73b3a9a87b4182a126e059c13e012c4719
Successful-Rosemary <tanki.online98@yandex.ru>
2020-01-09 20:02:42 +0300

    refactoring

---
+++ public/config.js
@@ -0,0 +1,10 @@
+window.BoardID = 1;
+window.GUID = "009bac39-d16d-4473-a0dc-7f8e73c9370d";
+window.host = "http://localhost:12348";
+window. api = "/api/WorkerBoard/GetWorkersSchedule";
+
+window. countString = 12;
+window.displayTime = 5000;
+
+window.height = 1080; // Высота монитора в пикселях
+window.heightString = 50;
